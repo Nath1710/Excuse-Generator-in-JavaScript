ab44bdae9078a303d0b6ed0a2dcbf9f07f04de22
Nath1710 <nathalyhernandez815@gmail.com>
2024-08-26 17:17:11 -0500

    Primer commit del proyecto

---
+++ script.js
@@ -0,0 +1,23 @@
+function generateExcuse() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying",
+  ];
+  let randomWho = Math.floor(Math.random() * who.length);
+  let randomAction = Math.floor(Math.random() * action.length);
+  let randomWhat = Math.floor(Math.random() * what.length);
+  let randomWhen = Math.floor(Math.random() * when.length);
+
+  return `${who[randomWho]} ${action[randomAction]} ${what[randomWhat]} ${when[randomWhen]}`;
+}
+function updatePhrase() {
+  let phrase = document.getElementById("excuse");
+  phrase.textContent = generateExcuse();
+}
+window.onload = updatePhrase;
